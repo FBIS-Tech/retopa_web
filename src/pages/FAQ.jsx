8f428f9b6773a575f5de7ff40726532432fa4261
osisamkay <osisami.oj@gmail.com>
2020-01-09 11:37:53 +0100

    fixed faq

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout/Layout"
 import "../scss/Terms.scss"
 import "../scss/Faq.scss"
-import { Input } from "antd"
+import { Input, Anchor } from "antd"
 import {
   Accordion,
   AccordionItem,
@@ -13,10 +13,14 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css"
 import "../scss/MerchantHero.scss"
 import { Accordion_FAQ_general } from "../components/Constants/FaqPage"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+const { Link } = Anchor
 
 const FAQ = () => {
-  const [focus, setFocus] = useState(false)
+  const [focus, setFocus] = useState(undefined)
+  useEffect(() => {
+    setFocus(window.innerHeight / 1.5)
+  }, [])
   return (
     <Layout>
       <div className="terms_header_container">
@@ -29,30 +33,23 @@ const FAQ = () => {
         <div className="faq_main_questions_group">
           <div className="faq_main_questions">
             <div className="faq_navlinks">
-              <Link to="FAQ" activeClassName="active">
-                General
-              </Link>
-              <Link
-                onFocus={() => {
-                  setFocus(true)
-                }}
-                ClassName={focus ? "active" : ""}
-                to="FAQ#safety"
-              >
-                Safety & Security
-              </Link>
-              <Link>Retopa’s Account</Link>
-              <Link>Topup and Bills</Link>
-              <Link>Retopa charges</Link>
-              <Link>Withhdrawals</Link>
-              <Link>Referral program</Link>
-              <Link>Mobile App</Link>
-              <Link>How does it work</Link>
-              <Link>Cashback</Link>
-              <Link to="ContactUs">More questions ?</Link>
+              <Anchor affix={false} targetOffset={focus}>
+                <Link href="#general" title="General" />
+                <Link href="#safety" title="Safety & Security" />
+                <Link href="#account" title="Retopa’s Account" />
+                <Link href="#topup" title="Topup and Bills" />
+                <Link href="#charges" title="Retopa charges" />
+                <Link href="#withdrwal" title="Withhdrawals" />
+                <Link href="#referal" title="Referral program" />
+                <Link href="#mobile" title="Mobile App" />
+                <Link href="#workings" title="How does it work" />
+                <Link href="#cashback" title="Cashback" />
+                <Link href="#howWorks" title="How does it work" />
+                <Link href="ContactUs" title="More questions ?" />
+              </Anchor>
             </div>
             <div className="main_questions">
-              <div className="accordion_view">
+              <div className="accordion_view" id="general">
                 <p className="accordion_view_title">WHAT IS RETOPA</p>
                 <div className="accordion">
                   <Accordion allowZeroExpanded={true}>
@@ -73,7 +70,7 @@ const FAQ = () => {
                   </Accordion>
                 </div>
               </div>
-              <div className="accordion_view">
+              <div className="accordion_view" id="safety">
                 <p className="accordion_view_title" id="safety">
                   SAFETY & SECURITY
                 </p>
@@ -117,7 +114,7 @@ const FAQ = () => {
                   </Accordion>
                 </div>
               </div>
-              <div className="accordion_view">
+              <div className="accordion_view" id="topup">
                 <p className="accordion_view_title">TOPUP & PAY BILLS</p>
                 <div className="accordion">
                   <Accordion allowZeroExpanded={true}>
@@ -138,7 +135,7 @@ const FAQ = () => {
                   </Accordion>
                 </div>
               </div>
-              <div className="accordion_view">
+              <div className="accordion_view" id="mobile">
                 <p className="accordion_view_title">MOBILE APP</p>
                 <div className="accordion">
                   <Accordion allowZeroExpanded={true}>
